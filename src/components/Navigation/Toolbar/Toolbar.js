35afed1392e8320e8d5e1df6c8be1d5ac8710aac
Roberto Alicata <roberto.alicata@gmail.com>
2018-02-06 12:53:27 +0100

    Add SideDrawer component

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,10 +1,19 @@
 import React from "react";
 
+import classes from "./Toolbar.css";
+import Logo from "../../Logo/Logo";
+import NavigationItems from "../NavigationItems/NavigationItems";
+import DrawerToggle from "../SideDrawer/DrawerToggle/DrawerToggle";
+
 const toolbar = props => (
-  <header>
-    <div>MENU</div>
-    <div>LOGO</div>
-    <nav>...</nav>
+  <header className={classes.Toolbar}>
+    <DrawerToggle clicked={props.drawerToggleClicked} />
+    <div className={classes.Logo}>
+      <Logo />
+    </div>
+    <nav className={classes.DesktopOnly}>
+      <NavigationItems />
+    </nav>
   </header>
 );
 
