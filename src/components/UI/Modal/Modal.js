8d60c2240b8ebb2c89ed2a4986b15345bc3de347
Roberto Alicata <roberto.alicata@gmail.com>
2018-02-08 12:05:07 +0100

    Change directory structure

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,12 +1,16 @@
 import React, { Component } from "react";
 
 import classes from "./Modal.css";
-import Aux from "../../../hoc/Aux";
+import Aux from "../../../hoc/Aux/Aux";
 import Backdrop from "../Backdrop/Backdrop";
 
 class Modal extends Component {
   componentWillUpdate() {
     console.log("[Modal] Will Update");
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    return nextProps.show !== this.props.show;
   }
 
   render() {
